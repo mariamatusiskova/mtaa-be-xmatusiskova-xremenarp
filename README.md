02cea96feddb03e2a90b3ef3bd0b904e6ef61b6e
mariamatusiskova <xmatusiskova@stuba.sk>
2024-03-26 15:46:15 +0100

    database configuration

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mtaa-be-xmatusiskova-xremenarp
 BE of REST and DB for subject MTAA
+
+Turn on Docker Desktop
+
+command in terminal to run project:
+```sh
+    docker compose up --attach node
+```
--- app/index.js
+++ app/index.js
@@ -1,8 +1,20 @@
 const config = require('./config.json');
 const express = require('express');
+const {MongoClient} = require('mongodb');
 
 const app = express();
 
+// Database connection for endpoints needs
+// by learn4you
+const mongoUri = process.env.MONGO_URI || config.mongo.uri;
+MongoClient.connect(mongoUri, {useUnifiedTopology: true}, (err, client) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(0);
+  }
+  const db = client.db(config.mongo.db);
+});
+//
 
 app.get('/', (req, res) => {
     res.send("Hello");
